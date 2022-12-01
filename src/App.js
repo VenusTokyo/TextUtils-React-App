3e21da1d4598acd5e31fdfd8e63e93bb733d3fdd
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2022-12-01 15:54:27 +0530

    video 5 adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
-let hui="harry";
+
 function App() {
   return (
     <>
-    {/* // this is a html in js ,this whole html is called jsx and js can be included in between in {}  */}
-     {/* /* class is writen by className */ }
-
-    {/* // this return can only return one tag */}
-    <nav>
-      <li>Home</li>
-      <li>about</li>
-      <li>contact</li>
-      <li>hui</li>
-      <h1>Hello {hui}</h1>
-    </nav>
-    <div className="container">
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus optio obcaecati exercitationem soluta molestias, aut placeat perferendis! Ab, et tempora nulla error, vero pariatur maiores exercitationem, aut accusamus inventore repellendus.</p>
-    </div>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">Text Utils</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">About</a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
