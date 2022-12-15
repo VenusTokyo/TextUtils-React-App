29b3cf1fcc16aa607824eb92425312b0c7347caf
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2022-12-15 17:20:51 +0530

    Updates

---
+++ precache-manifest.d6dd8e438d28c4663a646d24dcc11629.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef709aaed538df163584827c9f2273e",
+    "url": "/TextUtils-React-App/index.html"
+  },
+  {
+    "revision": "239511462340f1cf60d8",
+    "url": "/TextUtils-React-App/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ebae4d48ab9d28d876c3",
+    "url": "/TextUtils-React-App/static/js/2.157525e5.chunk.js"
+  },
+  {
+    "revision": "239511462340f1cf60d8",
+    "url": "/TextUtils-React-App/static/js/main.56830871.chunk.js"
+  },
+  {
+    "revision": "28cfcc1e644af2ffbecb",
+    "url": "/TextUtils-React-App/static/js/runtime~main.bb983724.js"
+  }
+]);
